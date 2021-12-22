a1958d7531e7477a98db19792ab54b2b5f466946
Rafael Silva <perigoso@riseup.net>
2021-12-22 17:20:33 +0000

    Rework release script and remove duplicated files
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- release.sh
+++ release.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 name="openinput-kicad-library"
-lib_files="./footprints ./3dmodels ./resources ./metadata.json"
+output_dir="release"
+lib_files="library/footprints library/3dmodels"
+
 
 # TODO write metadatas dynamically,
 # (pack side and kicad repo side)
 # rewrite in python
 
+# make output dir
+mkdir -p $output_dir/footprints $output_dir/3dmodels $output_dir/resources
+
+# Generate icon
+cairosvg --output-width 64 --output-height 64 assets/icon.svg -o ${output_dir}/resources/icon.png
+
+# Copy metadata
+cp metadata.json ${output_dir}/metadata.json
+
+# Copy lib files
+cp -r ${lib_files} ${output_dir}
+
 # Pack
-zip -r ./${name}.zip ${lib_files}
+cd ${output_dir}
+zip -r ./${name}.zip ./footprints ./3dmodels ./resources ./metadata.json
 
 # Compute checksum
 echo -n '"download_sha256": ' > ./${name}_info.txt
@@ -20,4 +35,7 @@ du -csb ./${name}.zip | grep total | sed 's/ *\stotal* *\(.*\)/\1/' >> ./${name}
 
 # Compute install size
 echo -n '"install_size": ' >> ./${name}_info.txt
-du -csb ${lib_files} | grep total | sed 's/ *\stotal* *\(.*\)/\1/' >> ./${name}_info.txt
+# du -csb ${lib_files} | grep total | sed 's/ *\stotal* *\(.*\)/\1/' >> ./${name}_info.txt
+
+# Return to root
+cd ..
