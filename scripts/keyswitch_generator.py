d184a6f5ee5fc58b0112ad8c0200ea142baa0e01
perigoso <perigoso@riseup.net>
2021-12-24 15:34:20 +0000

    add kailh choc mini switch
    Signed-off-by: perigoso <perigoso@riseup.net>

--- scripts/keyswitch_generator.py
+++ scripts/keyswitch_generator.py
@@ -8,7 +8,7 @@ from keycap import Keycap
 from switch import StabilizerCherryMX, SwitchAlpsMatias, SwitchCherryMX, \
                    SwitchHybridCherryMxAlps, SwitchKailhChocV1, \
                    SwitchHotswapKailh, SwitchHotswapKailhChocV1, SwitchKailhKH, \
-                   SwitchKailhNB
+                   SwitchKailhNB, SwitchKailhChocMini
 
 path3d = '${KICAD6_3RD_PARTY}/3dmodels/' \
          'com_github_perigoso_keyswitch-kicad-library/' \
@@ -164,6 +164,34 @@ def generate_switch_kailh_choc_v1(output_path):
         file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
 
 
+def generate_switch_kailh_choc_mini(output_path):
+    x_spacing = 18
+    y_spacing = 17
+    group = 'Switch_Keyboard_Kailh'
+    out_path = os.path.join(output_path, f'{group}.pretty')
+    if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
+            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
+            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
+            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
+            'ISOEnter270']
+
+    switches = []
+
+    switches.append(SwitchKailhChocMini(path3d=path3d))
+
+    for key in keys:
+        switches.append(SwitchKailhChocMini(path3d=path3d,
+                                          keycap=Keycap(x_spacing=x_spacing,
+                                                        y_spacing=y_spacing,
+                                                        **keycaps[key])))
+
+    for switch in switches:
+        file_handler = KicadFileHandler(switch)
+        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
+
+
 def generate_switch_kailh_kh(output_path):
     spacing = 19.05
     group = 'Switch_Keyboard_Kailh'
@@ -295,6 +323,7 @@ if __name__ == '__main__':
     generate_switch_cherry_mx(args.output)
     generate_switch_hybrid_cherry_mx_alps(args.output)
     generate_switch_kailh_choc_v1(args.output)
+    generate_switch_kailh_choc_mini(args.output)
     generate_switch_kailh_kh(args.output)
     generate_switch_kailh_nb(args.output)
     generate_switch_hotswap_kailh(args.output)
