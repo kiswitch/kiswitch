5f005d947a8cb5eb3374df11427ca19f1205996c
Rafael Silva <perigoso@riseup.net>
2022-11-06 04:30:26 +0000

    plugin: add base dialog

--- plugin/kswkip/plugin.py
+++ plugin/kswkip/plugin.py
@@ -2,6 +2,7 @@ import pcbnew
 import os
 import wx
 
+from .dialog import GeneratorDialog, ImporterDialog
 
 class KswKiPGenerator(pcbnew.ActionPlugin):
     def defaults(self):
@@ -11,6 +12,26 @@ class KswKiPGenerator(pcbnew.ActionPlugin):
         self.show_toolbar_button = True
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
 
+    def Run(self):
+
+        try:
+            dialog = GeneratorDialog()
+            dialog.ShowModal()
+
+        except Exception as e:
+            except_dialog = wx.MessageDialog(
+                None, str(e), 'Error', wx.OK)
+            except_dialog.ShowModal()
+            except_dialog.Destroy()
+
+            log_file = os.path.join(os.path.dirname(__file__), 'KswKiPGenerator.log')
+            with open(log_file, 'w') as f:
+                f.write(repr(e))
+
+        finally:
+            if 'dialog' in locals():
+                dialog.Destroy()
+
 
 class KswKiPImporter(pcbnew.ActionPlugin):
     def defaults(self):
@@ -19,3 +40,21 @@ class KswKiPImporter(pcbnew.ActionPlugin):
         self.description = 'Keyboard Keyswitch Importer'
         self.show_toolbar_button = True
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
+
+    def Run(self):
+        try:
+            dialog = ImporterDialog()
+            dialog.ShowModal()
+
+        except Exception as e:
+            except_dialog = wx.MessageDialog(
+                None, f'Exception: {e}', 'Error', wx.OK)
+            except_dialog.ShowModal()
+            except_dialog.Destroy()
+
+            log_file = os.path.join(os.path.dirname(__file__), 'KswKiPImporter.log')
+            with open(log_file, 'w') as f:
+                f.write(repr(e))
+
+        finally:
+            dialog.Destroy()
