5f8b1d36872d8a4e2a34f760108cec83c77af3bd
Rafael Silva <perigoso@riseup.net>
2022-11-08 00:56:09 +0000

    plugin/dialog: add confirmation dialog

--- plugin/kswkip/dialog.py
+++ plugin/kswkip/dialog.py
@@ -1,6 +1,19 @@
 
 import wx
 
+
+def get_confirmation_dialog(parent=None, message: str = '', title: str = 'Confirm') -> bool:
+    confirmation_dialog = wx.MessageDialog(
+        parent, message, title,
+        wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+    choice = confirmation_dialog.ShowModal()
+    confirmation_dialog.Destroy()
+
+    if choice == wx.ID_NO:
+        return False
+    return True
+
+
 class GeneratorDialog(wx.Dialog):
     def __init__(self, parent=None, project_path=None):
         wx.Dialog.__init__(
@@ -13,6 +26,7 @@ class GeneratorDialog(wx.Dialog):
         self.scrollWindow.FitInside()
         self.GetSizer().Layout()
         self.Fit()
+
 
 class ImporterDialog(wx.Dialog):
     def __init__(self, parent=None, project_path=None):
