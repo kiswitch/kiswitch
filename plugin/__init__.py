68769eb792fc537dc4f1bf52d57af1b2897b0071
Rafael Silva <perigoso@riseup.net>
2022-11-08 00:55:39 +0000

    plugin: add traceback to log

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,11 +1,12 @@
 try:
-    from .kswkip.plugin import KswKiPGenerator, KswKiPImporter
+	from .kswkip.plugin import KswKiPGenerator, KswKiPImporter
 
     KswKiPGenerator().register()
     # KswKiPImporter().register()
 
 except Exception as e:
     import os
+    import traceback
     log_file = os.path.join(os.path.dirname(__file__), 'KswKiP.log')
     with open(log_file, 'w') as f:
-        f.write(repr(e))
+        f.write(traceback.format_exc())
